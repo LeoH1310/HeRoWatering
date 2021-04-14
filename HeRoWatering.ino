6140fe08b616158db95415e31e7906ce9d2420a8
LeoH1310 <44778508+LeoH1310@users.noreply.github.com>
2021-04-14 22:12:33 +0200

    Projektdateien hinzufügen.

---
+++ HeRoWatering.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HeRoWatering.ino
+ Created:	14.04.2021 19:25:55
+ Author:	leonh
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
