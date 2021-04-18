e312d2e8a31b5ec57682678fb5e23a05c9474a05
LeoH1310 <44778508+LeoH1310@users.noreply.github.com>
2021-04-18 21:15:01 +0200

    added RTC, WiFi and Dusk2Dawn

---
+++ Credentials.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+extern const char* SSID = "Relax_2.4";
+extern const char* PASS = "leosabi782013";
+
+extern const float longitude = 49.02131768569845;
+extern const float latitude = 12.10671662706796;
+
+extern const int offsetMin = 30;
--- HeRoWatering.ino
+++ HeRoWatering.ino
@@ -1,15 +1,116 @@
 /*
  Name:		HeRoWatering.ino
  Created:	14.04.2021 19:25:55
- Author:	leonh
+ Author:	Leonhard Hesse
 */
+
+#include <Credentials.h>
+
+// Sleep Modes and Time for Arduino Nano 33 IoT
+#include <RTCZero.h>
+#include <time.h>
+
+// Libraries for Wifi
+#include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+
+// Sunrise and Sunset Library
+#include <Dusk2Dawn.h>
+
+// SQL MariaDB Library
+#include <MySQL_Generic_WiFiNINA.h>
+
+// ArduinoBlue Library
+#include <ArduinoBlue.h>
+
+// global variable for rtc
+RTCZero rtc;
+
+extern const int offsetMin;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+	// get extern variables
+	extern const float longitude;
+	extern const float latitude;
+	
+	// connect to WiFi network
+	int status = WL_IDLE_STATUS;
+	extern const char* SSID;
+	extern const char* PASS;
 
+	while (status != WL_CONNECTED) {
+		status = WiFi.begin(SSID, PASS);
+		delay(2000);
+	}
+
+	WiFi.lowPowerMode();	// enable WiFi low power mode
+
+	// RTC init
+	rtc.begin();
+	rtc.setEpoch(WiFi.getTime());	// set rtc from internet time
+	setMeasureAlarm();
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
   
+	//rtc.standbyMode();    // sleep until next alarm match
+}
+
+void ISR_RTC_Measurement() {
+	// ToDo: 
+	// - read plant sensors
+	// - read waterlevel sensor
+	// - logging sensor values und decision
+	// - watering is necessary and possible
+	//		yes:	- start watering
+	//				- set RTC for stopping watering
+	// 	    necessary but impossible:
+	//				- send Email notification - ERROR
+	//				- call setMeasureAlarm
+	//		no:		- call setMeasureAlarm
+}
+
+void ISR_RTC_StopWatering() {
+	// ToDo:
+	// - stop watering
+	// - call setMeasureAlarm
+}
+
+void setMeasureAlarm() {
+	// get todays sunrise and sunset
+	Dusk2Dawn regensburg(longitude, latitude, -2);
+	int sunriseMins = regensburg.sunrise(rtc.getYear(), rtc.getMonth(), rtc.getDay(), true);
+	int sunsetMins = regensburg.sunset(rtc.getYear(), rtc.getMonth(), rtc.getDay(), true);
+	int timeMins = getMinutesSinceMidnight();
+
+	// DEBUG:
+	char strSunrise[6];
+	Dusk2Dawn::min2str(strSunrise, sunriseMins);
+	char strSunset[6];
+	Dusk2Dawn::min2str(strSunset, sunsetMins);
+
+	// check what event is next
+	if (timeMins >= sunsetMins || timeMins < sunriseMins) {
+		// sunrise is next - set alarm to sunrise time + offset
+		rtc.setAlarmTime(sunriseMins / 60, sunriseMins % 60 + offsetMin, 0);
+	}
+	else 	{
+		// sunset is next - set alarm to sunset time - offset
+		rtc.setAlarmTime(sunsetMins / 60, sunsetMins % 60 - offsetMin, 0);
+	}
+	
+	rtc.enableAlarm(rtc.MATCH_HHMMSS);
+	rtc.attachInterrupt(ISR_RTC_Measurement);
+
+	//rtc.standbyMode();
+}
+
+int getMinutesSinceMidnight() {
+	int minutes;
+	minutes = rtc.getHours() * 60;
+	minutes += rtc.getMinutes();
+	return minutes;
 }
